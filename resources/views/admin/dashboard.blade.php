2811e4b84039d8a6e3546d65e3b78a9bcbd4eb60
admin <waleed.aptech.use@gmail.com>
2026-07-04 00:21:12 -0700

    done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    admin
-    <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
+@extends('admin.master')
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-</body>
-</html>
+@section('content')
+
+hello
+
+@endsection
